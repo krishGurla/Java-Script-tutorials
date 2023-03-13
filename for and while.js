9a6669834858cabd1fa8ae2e9e4a90471d4f181d
KRISH <126510888+krishGurla@users.noreply.github.com>
2023-03-13 23:43:24 +0530

    Create for and while.js

---
+++ for and while.js
@@ -0,0 +1,13 @@
+//LET US TRY TO UNDERSTAND THE DIFFERENCE BETWEEN FOR LOOP AND WHILE LOOP
+
+        //FOR LOOP
+       
+       let i = 0;
+       for(i=0;i<10;i++){
+            console.log(i);
+        };
+        //WHILE LOOP
+        while(i<5){
+            console.log(i);
+            i++;
+        };
