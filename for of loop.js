031b3633749c4a9c34c11f656e9b0274b4db692f
KRISH <126510888+krishGurla@users.noreply.github.com>
2023-03-13 23:27:51 +0530

    Create for of loop.js

---
+++ for of loop.js
@@ -0,0 +1,8 @@
+/FOR OF LOOP TUTORIAL
+        const loop = ["for in","for of"];
+        let x = " ";
+        for(z of loop){
+            x+=z + " ";
+        };
+        console.log(x);
+        
