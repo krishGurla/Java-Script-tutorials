b45f99bd064b1533d8e6586ef97e1470bc803b8b
KRISH <126510888+krishGurla@users.noreply.github.com>
2023-03-13 23:22:18 +0530

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Switch Statement and structures</h1>
-    <script>
-        var fruitType = prompt('which fruit do you want:');
+var fruitType = prompt('which fruit do you want:');
         switch (fruitType) {
             case 'apple':
                 console.log('A dozen of apple costs 70 rupees');
@@ -27,8 +14,3 @@
 
         }
         console.log('enough of coding now go to sleep');
-
-    </script>
-</body>
-
-</html>
